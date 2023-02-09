811bda5de70b2f43cdb4b4f68ef762150e43ab41
unknown <dungprof8.0@gmail.com>
2023-02-09 16:46:51 +0700

    update client

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -10,71 +10,81 @@ function Header() {
 
     return (
         <div className="header">
-        <div className="header_item1">
-            <div className="header_item1-1">
-                <div className="header_item_1-1_btn">
-                    <span>Kênh Người Bán</span>
-                </div>
-                <div className="header_item_1-1_btn">
-                    <span>Tải Ứng Dụng</span>
-                </div>
-                <div className="header_item_1-1_btn">
-                    <span>Language</span>
-                    <FontAwesomeIcon icon={faCaretDown} />
-                </div>
-            </div>
-
-            <div className="header_item1-2">
-                <div className="header_item_1-2_btn">
-                    <div className="wrap_btn_login-logout">
-                        <a href="/">
-                            <span>Login</span>
-                        </a>
-                        <p>/</p>
-                        <a href="/">
-                            <span>Sign Up</span>
-                        </a>
+            <div className="header_item1">
+                <div className="header_item1-1">
+                    <div className="header_item_1-1_btn">
+                        <span>Kênh Người Bán</span>
+                    </div>
+                    <div className="header_item_1-1_btn">
+                        <span>Tải Ứng Dụng</span>
+                    </div>
+                    <div className="header_item_1-1_btn">
+                        <span>Language</span>
+                        <FontAwesomeIcon icon={faCaretDown} />
                     </div>
                 </div>
-                <div className="header_item_1-2_btn">
-                    <span className="text-thongbao">Thông Báo</span>
-                    <FontAwesomeIcon className="thongbao-icon" icon={faBell} />
-                </div>
-                <div className="header_item_1-2_btn">
-                    <a href="/">
-                        <FontAwesomeIcon icon={faSquareYoutube} />
-                    </a>
-                </div>
-                <div className="header_item_1-2_btn">
-                    <a href="/">
-                        <FontAwesomeIcon icon={faFacebookF} />
-                    </a>
-                </div>
-                <div className="header_item_1-2_btn">
-                    <a href="/">
-                        <FontAwesomeIcon icon={faInstagram} />
-                    </a>
-                </div>
-                <div className="header_item_1-2_btn">
-                    <a href="/">
-                        <FontAwesomeIcon icon={faTwitter} />
-                    </a>
-                </div>
 
+                <div className="header_item1-2">
+                    <div className="header_item_1-2_btn">
+                        <div className="wrap_btn_login-logout">
+                            <a href="/">
+                                <span>Login</span>
+                            </a>
+                            <p>/</p>
+                            <a href="/">
+                                <span>Sign Up</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="header_item_1-2_btn">
+                        <span className="text-thongbao">Thông Báo</span>
+                        <FontAwesomeIcon className="thongbao-icon" icon={faBell} />
+                    </div>
+                    <div className="header_item_1-2_btn">
+                        <a href="/">
+                            <FontAwesomeIcon icon={faSquareYoutube} />
+                        </a>
+                    </div>
+                    <div className="header_item_1-2_btn">
+                        <a href="/">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </a>
+                    </div>
+                    <div className="header_item_1-2_btn">
+                        <a href="/">
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                    </div>
+                    <div className="header_item_1-2_btn">
+                        <a href="/">
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="header_item2">
+                <div className="wrap_header_item2">
+                    <div className="header_logo">
+                        <a className="wrap_logo" href="/">
+                            <h1 className="header_logo_item">TECH</h1>
+                            <h1 className="header_logo_item">STORE</h1>
+                        </a>
+                    </div>
+                    <div className="header_search">
+                        {/* <div className="search">
+                        <input className="search_input"></input>
+                    </div> */}
+                        <div className='search'>
+                            {/* <div className="wrapiconsearch"><FontAwesomeIcon className='iconsearch' icon={faMagnifyingGlass} /></div> */}
+                            <input placeholder='Tìm kiếm bài hát, nghệ sĩ, lời bài hát...' className='search_input' ></input>
+                        </div>
+                        <div className="search_btn"></div>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div className="header_item2">
-            <div className="wrap_header_item2">
-                <div className="header_logo">
-                    <a className="wrap_logo" href="/">
-                    <h1 className="header_logo_item">TECH</h1>
-                    <h1 className="header_logo_item">STORE</h1>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
     )
 }
 
