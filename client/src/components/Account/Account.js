8c9185bf9de7c4421ff1367d4d97a34240101f46
unknown <dungprof8.0@gmail.com>
2023-02-12 15:07:04 +0700

    update scss

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -1,44 +1,35 @@
 import { Link } from 'react-router-dom'
 import { useUserData } from '../../Context'
-import { logout } from '../../utils/Auth'
 import './Account.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser} from "@fortawesome/free-solid-svg-icons"
 function Account() {
     // Fetch user Data when app first load
-    const [user] = useUserData()
-    function handleLogout() {
-        logout()
-            .then(data => {
-                console.log(data)
-                localStorage.clear()
-                window.location.reload()
-            })
-            .catch(error => {
-                alert(error.message)
-            })
-    }
+    const [user, dispatchUser] = useUserData()
     console.log(user)
     if (user) return (
-        <div className='header-account logon'>
+        <>
             <div className='drop-list-main'>
+                <FontAwesomeIcon icon={faUser} />
                 <span>{user.username}</span>
             </div>
             <div className='drop-list-item'>
-                <div><Link to='#'>Tài khoản</Link></div>
-                <div><Link to='/change-password'>Đổi mật khẩu</Link></div>
-                <div onClick={handleLogout}><Link to='#'>Đăng xuất</Link></div>
+                <span>Tài Khoảng Của Tôi</span>
+                <span>Đổi Mật Khẩu</span>
+                <span>Đăng xuất</span>
             </div>
-        </div>
+        </>
     )
     else return (
-        <div className='header-account'>
-            <Link className='login-btn' to='/login'>
+        <>
+            <Link to='/login'>
                 <span>Đăng nhập</span>
             </Link>
-            {/* <p>/</p> */}
-            <Link className='register-btn' to='/register'>
+            <p>/</p>
+            <Link to='/register'>
                 <span>Đăng ký</span>
             </Link>
-        </div>
+        </>
     )
 }
 
