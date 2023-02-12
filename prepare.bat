54b6f44f04f6bb901313a5d8892c79fddb3237c6
HOAI AN <98139595+HOAIAN2@users.noreply.github.com>
2023-02-12 19:47:37 +0700

    Add Random KEY

--- prepare.bat
+++ prepare.bat
@@ -1,13 +1,5 @@
 @ECHO OFF
-DEL .\server\.env
-ECHO SERVER_PORT=4000>>.\server\.env
-ECHO DB_HOST="localhost">> .\server\.env
-ECHO DB_PORT=3306>> .\server\.env
-ECHO DB_USER="admin">> .\server\.env
-ECHO DB_PASSWORD="password">> .\server\.env
-ECHO DB_NAME="store">> .\server\.env
-ECHO ACCESS_TOKEN_SECRET="Your SECRET ACCESS TOKEN">> .\server\.env
-ECHO REFRESH_TOKEN_SERCET="Your REFRESH TOKEN SERCET">> .\server\.env
+CALL node random
 CD server
 ( CALL yarn install ) || ( CALL npm i )
 CD ../client
