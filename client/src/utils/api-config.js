4e8e4bb0ea1c415c82bb5d81e18467cc25cf900a
HOAI AN <98139595+HOAIAN2@users.noreply.github.com>
2023-02-18 09:46:06 +0700

    add auto change baseURL, add products to database

--- client/src/utils/api-config.js
+++ client/src/utils/api-config.js
@@ -1,17 +1,24 @@
 import axios from 'axios'
 
+const devOrigin = [
+  'http://127.0.0.1:3000',
+  'http://localhost:3000'
+]
+let baseURL = ''
+if (devOrigin.includes(window.location.origin)) baseURL = 'http://localhost:4000/api/'
+else baseURL = `${window.location.origin}/api`
 const request = axios.create({
-    baseURL: 'http://localhost:4000/api/'
+  baseURL: baseURL
 })
 
 export default request
 
 
 const api_laptop = [
-    "https://tiki.vn/api/v2/products?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&q=laptop+gaming&page=2",
-    "https://tiki.vn/api/v2/products?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&q=laptop+gaming",
-    "https://tiki.vn/api/v2/products?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&q=laptop+gaming&page=3",
-    "https://tiki.vn/api/v2/products?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&q=maxbook&sort=price,desc",
+  "https://tiki.vn/api/v2/products?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&q=laptop+gaming&page=2",
+  "https://tiki.vn/api/v2/products?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&q=laptop+gaming",
+  "https://tiki.vn/api/v2/products?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&q=laptop+gaming&page=3",
+  "https://tiki.vn/api/v2/products?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&q=maxbook&sort=price,desc",
 ]
 const api_phone = [
   "https://tiki.vn/api/personalish/v1/blocks/listings?limit=40&include=advertisement&aggregations=2&trackity_id=87bffd52-cc74-68e0-5fd3-239a5f9665bf&category=1789&page=1&urlKey=dien-thoai-may-tinh-bang",
