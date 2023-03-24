b364932608edd64633437549d6b378c5f690c321
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-03-24 12:54:41 +0700

    Remove comments

--- client/src/components/home_content/content/HotProducts.js
+++ client/src/components/home_content/content/HotProducts.js
@@ -7,45 +7,6 @@ import BackageSlice from "@danghung_dung/slice_items"
 function HotProducts() {
     const [products, setProducts] = useState([])
     const container = useRef()
-    // function test(element, index) {
-    //     const valuetransforms = element.attributes.valuetransfrom.value
-    //     const stringtransfrom = `translate(calc(${valuetransforms}% - 100%), 0px)`
-    //     element.attributes.valuetransfrom.value = valuetransforms - 100
-    //     element.style.transform = stringtransfrom
-    // }
-
-    // useEffect(() => {
-    //     if (!products.length) return
-    //     if (products.length != 0) {
-    //         const [...a] = document.querySelectorAll(".hot-item")
-    //         a.every((element, index) => {
-    //             if (index < 3) {
-    //                 element.setAttribute('valuetransfrom', -100 * (4 - (index + 1)))
-    //                 element.style.transform = `translate(calc(-100%*${4 - (index + 1)}), 0px)`
-    //                 return true
-    //             } else {
-    //                 element.setAttribute('valuetransfrom', 0)
-    //                 element.style.transform = `translate(0%, 0px)`
-    //                 return true
-    //             }
-    //         })
-    //         setInterval(() => {
-    //             const [...a] = document.querySelectorAll(".hot-item")
-    //             a.every((element, index) => {
-    //                 if (index < 4) {
-    //                     test(element, index)
-    //                     return true
-    //                 }
-    //             })
-    //             setTimeout(() => {
-    //                 a[0].style.transform = `translate(0%, 0px)`
-    //                 a[0].attributes.valuetransfrom.value = 0
-    //                 document.querySelector(".hot-item-content").appendChild(a[0])
-    //             }, 1000)
-    //         }, 5000)
-
-    //     }
-    // }, [products])
     function formatPrice(price) {
         return `${price.toLocaleString('vi')} VNĐ`
     }
