2931cc923e867acd72e345ebd8ff8fca4b2a2c77
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-03-26 08:19:01 +0700

    Edit 404 page

--- client/src/Page/errors/NotFound.js
+++ client/src/Page/errors/NotFound.js
@@ -2,18 +2,24 @@ import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../../components/header/Header'
 import './NotFound.scss'
-
+import languages from './Languages/NotFound.json'
+import HotProducts from '../../components/home_content/content/HotProducts'
 function NotFound() {
+    let language = languages.en
+    if (navigator.language === 'vi') language = languages.vi
     useEffect(() => {
-        document.title = 'Không tìm thấy'
+        document.title = language.title
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
         <div className='not-found'>
             <Header />
             <div className='not-found__contents'>
-                <div>Rất tiếc, trang này hiện không tồn tại</div>
-                <div>Liên kết có thể đã bị gỡ bỏ.<Link to='/'> Về trang chủ</Link></div>
+                <div>404</div>
+                <div>{language.content}</div>
+                <div><Link to='/'> Về trang chủ</Link></div>
             </div>
+            <HotProducts />
         </div>
     )
 }
