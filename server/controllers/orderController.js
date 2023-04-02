906c180c4b5f070bb38e44dff60d84228e21c270
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-04-02 17:48:30 +0700

    Add flow for backend order api

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -2,55 +2,52 @@ const { readAccessToken } = require("../controllers/authController")
 const { orders, getOrder, addOrder, addOrderDetail, updateOrderDetail } = require("../cache")
 const orderErrors = require("./orderErrors.json")
 
-
+// [POST create-order]
 async function createOrder(req, res) {
     let errorMessages = orderErrors.en
     const language = req.headers["accept-language"]
     if (language === 'vi') errorMessages = orderErrors.vi
     const token = req.headers['authorization'].split(' ')[1]
     const user = readAccessToken(token)
-    // let time = new Date();
-    // time = time.toLocaleTimeString('it-IT')
-    const data = {
-        userID: user.id,
-        productID: req.body.productID,
-        productQuantity: req.body.productQuantity,
-    }
-    // Nếu cùng 1 lúc 1 user cho tồn tại nhiều order thì không cần check này nọ, cứ insert luôn
-    // Đoạn dưới từ từ handle
-    const dataFormat = formatdata(data);
-    if (!dataFormat) return res.status(400);
-    const orderinvalit = orders.find((item) => {
-        console.log(item.userID, user.id, item.userID === user.id)
-        if (item.userID === user.id) {
-            return item
-        }
-    })
-    console.log(orders)
-    console.log(orderinvalit)
-
-    if (orderinvalit) {
-        orderinvalit.products.every(async (item) => {
-            if (item.productID === data.productID) {
-                const newquantity = item.quantity + 1;
-                const order = await updateOrderDetail(newquantity, item.orderID, data.productID)
-                orderinvalit.setProduct(data.productID, quantity)
-                res.status(200).json(orderinvalit)
-            }
-            return true
-        })
-        const order = await addOrderDetail(orderinvalit.orderID, data.productID, data.productQuantity)
-        res.status(200).json(order)
-    } else {
-        const orderID = await addOrder(data.userID)
-        const order = await addOrderDetail(orderID, data.productID, data.productQuantity)
-        console.log(order, orders)
-        res.status(200).json(order)
-    }
-
+}
+// [POST add-product]
+async function addProduct(req, res) {
+    let errorMessages = orderErrors.en
+    const language = req.headers["accept-language"]
+    if (language === 'vi') errorMessages = orderErrors.vi
+    const token = req.headers['authorization'].split(' ')[1]
+    const user = readAccessToken(token)
+    //
+}
+// [POST update-product]
+async function updateProduct(req, res) {
+    let errorMessages = orderErrors.en
+    const language = req.headers["accept-language"]
+    if (language === 'vi') errorMessages = orderErrors.vi
+    const token = req.headers['authorization'].split(' ')[1]
+    const user = readAccessToken(token)
+    //
+}
+// [POST remove-product]
+async function removeProduct(req, res) {
+    let errorMessages = orderErrors.en
+    const language = req.headers["accept-language"]
+    if (language === 'vi') errorMessages = orderErrors.vi
+    const token = req.headers['authorization'].split(' ')[1]
+    const user = readAccessToken(token)
+    //
+}
+// [POST make-payment]
+async function makePayment(req, res) {
+    let errorMessages = orderErrors.en
+    const language = req.headers["accept-language"]
+    if (language === 'vi') errorMessages = orderErrors.vi
+    const token = req.headers['authorization'].split(' ')[1]
+    const user = readAccessToken(token)
+    //
 }
 
-
+// middleware,..
 function formatdata(data) {
     const rs = data;
     const a = Object.keys(rs).every((item) => {
@@ -68,4 +65,8 @@ function formatdata(data) {
 
 module.exports = {
     createOrder,
+    addProduct,
+    updateProduct,
+    removeProduct,
+    makePayment
 }
--- server/routes/order.js
+++ server/routes/order.js
@@ -2,7 +2,9 @@ const express = require("express")
 const rounter = express.Router()
 const { orderController, authController } = require("../controllers")
 
-rounter.post("/createorder", authController.authenticateToken, orderController.createOrder)
-
+rounter.post('/create-order', authController.authenticateToken, orderController.createOrder)
+rounter.post('/add-product', authController.authenticateToken, orderController.addProduct)
+rounter.post('/update-product', authController.authenticateToken, orderController.updateProduct)
+rounter.post('/remove-product', authController.authenticateToken, orderController.removeProduct)
 
 module.exports = rounter
