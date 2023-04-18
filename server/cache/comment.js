cda0a0933a5b22be57dc31a87176798bec977f8f
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-04-18 12:18:16 +0700

    Add avatar, rate to Comment

--- server/cache/comment.js
+++ server/cache/comment.js
@@ -24,24 +24,29 @@ const { pool } = require('./database')
 // }
 
 async function queryComments(productID, startIndex, sortMode) {
+    let replace = '>'
+    if (sortMode === 'DESC') replace = '<'
     const comments = []
     try {
         const queryString = [
-            'SELECT comment_id, first_name, last_name, product_id, comment, comment_date FROM comments',
+            'SELECT comment_id, users.avatar, first_name, last_name, comments.product_id, comment, rate, comment_date FROM comments',
             'JOIN users ON users.user_id = comments.user_id',
-            'WHERE product_id = ? AND comment_id > ?',
+            'JOIN ratings ON ratings.user_id = comments.user_id AND ratings.product_id = comments.product_id',
+            `WHERE comments.product_id = ? AND comment_id ${replace} ?`,
             `ORDER BY comment_id ${sortMode}`,
             'LIMIT 10'
         ].join(' ')
         const [rows] = await pool.query(queryString, [productID, startIndex])
         rows.forEach(row => {
             const commentID = row['comment_id']
+            const avatar = row['avatar']
             const firstName = row['first_name']
             const lastName = row['last_name']
             const productID = row['product_id']
             const commentContent = row['comment']
+            const rate = row['rate']
             const commentDate = row['comment_date']
-            const comment = new Comment(commentID, firstName, lastName, productID, commentContent, commentDate)
+            const comment = new Comment(commentID, avatar, firstName, lastName, productID, commentContent, rate, commentDate)
             comments.push(comment)
         })
         return comments
--- server/models/comment.js
+++ server/models/comment.js
@@ -1,10 +1,12 @@
 class Comment {
-    constructor(commentID, userFirstName, userLastName, productID, comment, commentDate) {
+    constructor(commentID, avatar, userFirstName, userLastName, productID, comment, rate, commentDate) {
         this.commentID = commentID
+        this.avatar = avatar
         this.userFirstName = userFirstName
         this.userLastName = userLastName
         this.productID = productID
         this.comment = comment
+        this.rate = rate
         this.commentDate = new Date(commentDate)
     }
 }
